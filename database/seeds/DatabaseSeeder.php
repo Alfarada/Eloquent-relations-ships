5b7c2ca82d48b9a994ffb8af8bc5e721092d20b9
Alfredo <alfaradayepez@gmail.com>
2020-07-04 13:04:59 -0500

    Creando valores por defecto en los factorys para cada uno de los modelos y configurando  los seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\{Group, Level, User, Profile, Location};
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(Group::class, 3)->create();
+
+        factory(Level::class)->create(['name' => 'Oro']);
+        factory(Level::class)->create(['name' => 'Plata']);
+        factory(Level::class)->create(['name' => 'Bronce']);
+
+        factory(User::class, 5)->create()->each( function ($user){
+            
+            $profile = $user->profile()->save(factory(Profile::class)->make());
+            $profile->location()->save(factory(Location::class)->make());
+            $user->groups()->attach($this->array(rand(1,3)));
+            $user->image()->save(factory(Image::class)
+                ->make([
+                    'url' => 'https://lorempixel.com/90/90/'
+                ]));
+        });
+    }
+
+    public function array($max)
+    {
+        $values = [];
+
+        for ($i=1; $i < $max; $i++) { 
+                $values = $i;
+        }
+
+        return $values; 
     }
 }
