a7750fcaf8cfe4c6c15109d41c500507a66d5681
Alfredo <alfaradayepez@gmail.com>
2020-07-04 19:22:58 -0500

    agregando bootstrap al perfil de usuario

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -8,20 +8,37 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
         <!-- Styles -->
     </head>
     <body>
-        <div class="container">
+        <div class="container my-3 pt-3 shadow">
             <div class="row">
                 <div class="col-12">
+                    <img src=" {{ $user->image->url }}" class="float-left rounded-circle mr-2" >
                     <h1> {{ $user->name }}</h1>
                     <h3> {{ $user->email }}</h3>
                     <ul>
                         <li><strong> Instagram </strong> : {{ $user->profile->instagram }}</li>
                         <li><strong> Github </strong> : {{ $user->profile->github }}</li>
                         <li><strong> Website </strong> : {{ $user->profile->web }}</li>
+                        <li><strong> Contry </strong> : {{ $user->location->country }}</li>    
+                        @if ($user->level)
+                            <li><strong> Level </strong> : <a href="#"> {{ $user->level->name }} </a> </li>
+                        @else
+                            <li><strong> --- </strong></li>
+                        @endif 
                     </ul>
+                    <hr>
+                    <p><strong> Groups </strong> : 
+                        @forelse ($user->groups as $group)
+                            <span class="badge badge-primary"> {{ $group->name }} </span>
+                        @empty
+                            <em>Does not belong to any group</em>
+                        @endforelse
+                    </p>
+                    <hr>
+
                 </div>
             </div> 
         </div>
